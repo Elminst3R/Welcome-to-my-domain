95d49d4bc0bb2850e28703eee0cb482b90f0864f
Elminst3R <baldur5@yahoo.com>
2020-10-09 00:02:47 -0500

    init

---
+++ myscript.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+  var element = document.getElementById("myDropdown");
+  element.classList.toggle("dropdown-contentfix");
+}
