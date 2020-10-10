d3e51567fcd4985a9b605094c15bd4734214069b
Elminst3R <baldur5@yahoo.com>
2020-10-10 01:23:02 -0500

    added formspree and validation

--- myscript.js
+++ myscript.js
@@ -3,3 +3,13 @@ function myFunction() {
   var element = document.getElementById("myDropdown");
   element.classList.toggle("dropdown-contentfix");
 }
+
+function validateForm() {
+  var x = document.forms["comments"]["firstname"].value;
+  var x = document.forms["comments"]["lastname"].value;
+  var x = document.forms["comments"]["subject"].value;
+  if (x == "") {
+    alert("First Name, Last Name, and Comments section must be filled out. Thank you!");
+    return false;
+  }
+}
